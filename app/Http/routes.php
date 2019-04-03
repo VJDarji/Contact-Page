9490ebac6ed2610eb2e1f6b201af236037992ba3
Vaibhav Darji <vaibhavdarji1313@gmail.com>
2019-04-03 16:44:28 -0400

    Inital commit with changed code

---
+++ app/Http/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::post('/thank_you', 'ContactController@index');
